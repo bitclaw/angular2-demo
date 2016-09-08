356a92c99f06fa56fe209725be5f7f82e2e42823
Daniel C <bitclaw@gmail.com>
2016-09-08 15:07:57 -0500

    Add files.

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 Run the following commands when downloading repo for the first time:
 
+
+```shell
+# Update node deps
 npm i
+# Get selenium and webdriver
 npm run webdriver:update
+# Start app
+npm start
+# Run protractor e2e tests
+npm run e2e
+# Run tests in console
+npm test
+```
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <div [style.color] = 'mycolor'>{{name}}</div>
+        <button (click) = "changeColor()">Change color</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+
+    // [] means property binding - Component to DOM
+    // () means event binding - DOM to Component
+
+    title = 'Customer App';
+    name = 'Daniel';
+    mycolor = 'blue';
+
+
+    changeColor() {
+        this.mycolor = this.mycolor === 'blue' ? this.mycolor = 'red' : this.mycolor = 'blue';
+    }
+
+}
